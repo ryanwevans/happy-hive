ef0448d8fe67fad1622bf39705d9ece44e22285d
Ryan <ryanwevans@icloud.com>
2019-07-19 09:01:21 -0700

    Add api namespaced routes for chores, rewards and acheivers

---
+++ happy-hive-api/app/controllers/api/acheivers_controller.rb
@@ -0,0 +1,4 @@
+class Api::AcheiversController < ApplicationController
+
+
+end
--- happy-hive-api/config/routes.rb
+++ happy-hive-api/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    resources :chores, except: [:new, :edit]
+
+    resources :rewards, except: [:new, :edit]
+
+    resources :acheivers, except: [:new, :edit]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
